39e9edc0b53a2a0b55d7d187e774e2d8f2c6f59a
rainskin <homemadepunk@yandex.ru>
2025-02-04 09:48:55 +0500

    Added important methods in Service constructor

--- src/db/service.py
+++ src/db/service.py
@@ -5,11 +5,18 @@ class Service:
 
     def __init__(self, db: Database):
         self.collection = db['service']
+        self.welcome_text = "✅"
+        self.add_welcome_text()
+        self.set_up_user_registration()
 
-    def add_welcome_text(self, text: str):
+    def add_welcome_text(self):
+        doc = self.collection.find_one({'welcome_text': True})
+        if doc:
+            return
+
         doc = {
             "welcome_text": True,
-            "text": text
+            "text": self.welcome_text
         }
 
         self.collection.insert_one(doc)
@@ -23,3 +30,14 @@ class Service:
 
         doc = self.collection.find_one({'main_settings': True})
         return doc['enable_user_registration'] if doc else None
+
+    def set_up_user_registration(self):
+        doc = self.collection.find_one({'main_settings': True})
+
+        if doc:
+            return
+
+        self.collection.insert_one({
+            'main_settings': True,
+            'enable_user_registration': True
+        })
